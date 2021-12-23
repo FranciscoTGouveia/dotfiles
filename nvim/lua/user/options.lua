d5bf867c717bbe943a835ab110fc3998a23568ff
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2021-12-23 12:00:16 +0000

    Start lua config

---
+++ nvim/lua/user/options.lua
@@ -0,0 +1,9 @@
+-- Indentation Options
+vim.opt.autoindent = true -- New lines inherit the indentation of previous lines
+vim.opt.shiftwidth = 4 -- When shifting, indent using four spaces
+vim.opt.smarttab = true -- Insert tabstop number of spaces when the tab key is pressed
+vim.opt.tabstop = 4 -- Indent using four spaces
+
+-- Search Options
+
+vim.opt.number = true -- Showthe line numbers onthe sidebar 
