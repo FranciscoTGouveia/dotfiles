f5111bf35fcf7d52ac323096a193b1768e2457e0
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2021-12-23 12:31:20 +0000

    Add neovim options config

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,9 +1,44 @@
+local set = vim.opt -- Create a local variable to reuse code
+
 -- Indentation Options
-vim.opt.autoindent = true -- New lines inherit the indentation of previous lines
-vim.opt.shiftwidth = 4 -- When shifting, indent using four spaces
-vim.opt.smarttab = true -- Insert tabstop number of spaces when the tab key is pressed
-vim.opt.tabstop = 4 -- Indent using four spaces
+set.autoindent = true -- New lines inherit the indentation of previous lines
+set.shiftwidth = 4 -- When shifting, indent using four spaces
+set.smarttab = true -- Insert tabstop number of spaces when the tab key is pressed
+set.tabstop = 4 -- Indent using four spaces
+
 
 -- Search Options
+set.hlsearch = true -- Enable search highlightning
+set.ignorecase = true -- Ignore case when searching
+set.incsearch = true -- Incremental search that shows partial matches
+set.smartcase = true -- Automatically switch search to case-sensitive if the query has an uppercase letter
 
-vim.opt.number = true -- Showthe line numbers onthe sidebar 
+
+-- Text Rendering Options
+set.linebreak = true -- Avoid wrapping a line in the middle of a word
+set.scrolloff = 1 -- The number of screen lines to keep aboce and below of the cursor
+set.sidescrolloff = 5 -- The number of columns to keep to the left and right of the cursor
+set.wrap = true -- Enable line wrapping
+
+
+-- User Interface Options
+set.laststatus = 2 -- Always display the status bar
+set.ruler = true -- Always show cursor position
+set.wildmenu = true -- Display command line's tab completion options as a menu
+set.tabpagemax = 10 -- Maximum number of tab pages that can be opened from the command line 
+set.number = true -- Show the line numbers on the sidebar
+set.relativenumber = true -- Show line number on the current line and relative numbers on all other lines
+set.visualbell = true -- Flash the screen on errors
+set.mouse = 'a' -- Enable mouse scrolling (and possible resizing) for all modes 
+
+
+-- Code Folding Options
+set.foldmethod = 'indent' -- "Fold based on indentation levels
+
+
+-- Miscellaneous Options
+set.backspace= 'indent,eol,start' -- Allow backspacing ovre indentation linebreaks and insertion start
+set.backupdir= '~/.cache/nvim'	-- Directory to store backup files
+set.confirm = true -- Display a confirmation dialog when writing and quitting
+set.hidden = true -- Hide files in the background instead of closing them
+set.history = 1000 -- Set the undo limit to 1000
