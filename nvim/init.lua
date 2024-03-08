68016221de86506c0287060ee32970fc68d4b6c1
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2024-03-08 01:23:03 +0000

    Clean neovim plugins and lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -17,26 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
-
-local opts = {}
-
+require("vim-options")
 require("lazy").setup("plugins")
 
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-vim.keymap.set("n", "<leader>w", builtin.live_grep, {})
-
-vim.keymap.set("n", "<leader>d", ":Neotree<CR>", {})
-
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-    ensure_installed = { "lua", "python", "cpp", "c", "java" },
-    highlight = { enable = true },
-    indente = { enable = true }
-})
-
-require("kanagawa").setup()
-vim.cmd.colorscheme "kanagawa-dragon"
-require("telescope").setup()
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,12 @@
 return { 
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate" 
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+        ensure_installed = { "lua", "python", "cpp", "c", "java" },
+        highlight = { enable = true },
+        indente = { enable = true }
+        })
+    end
 }    
