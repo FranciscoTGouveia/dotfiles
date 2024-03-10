5b6df3e21e2307850054ece4e41d1cf4dbb54853
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2024-03-10 21:33:40 +0000

    Add which-key plugin to neovim

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,35 +1,27 @@
 return {
     {
         "williamboman/mason.nvim",
+        lazy = false,
         config = function ()
             require("mason").setup()
         end
     },
     {
         "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "arduino_language_server",
-                    "clangd",
-                    "marksman",
-                    "pyright",
-                    "sqlls",
-                    "tsserver",
-                    "jdtls"
-                }
-            })
-        end
+        lazy = false,
+        opts = {auto_install = true}
     },
     {
         "neovim/nvim-lspconfig",
+        lazy = false,
         config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.pyright.setup({})
-            lspconfig.clangd.setup({})
-            lspconfig.jdtls.setup({})
+            lspconfig.lua_ls.setup({ capabilities = capabilities
+            })
+            lspconfig.pyright.setup({capabilities = capabilities})
+            lspconfig.clangd.setup({capabilities = capabilities})
+            lspconfig.jdtls.setup({capabilities = capabilities})
             vim.keymap.set("n", "<leader>d", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, {})
             vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, {})
